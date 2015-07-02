e99db5b9d0c83490836d20b41a1b864979f36e10
hcz <h.cz@qq.com>
2015-07-02 13:06:01 +0800

    working

--- bookstore_cgi.hpp
+++ bookstore_cgi.hpp
@@ -3,22 +3,20 @@
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
 
-#include "cgiutil.hpp"
-#include "bookstore_model.hpp"
-
 #include "reflection++/visitor_chain.hpp"
 #include "reflection++/visitor/strtree.hpp"
 #include "reflection++/visitor/json.hpp"
 #include "reflection++/visitor/bson.hpp"
 #include "reflection++/visitor/bson_view.hpp"
-#include "reflection++/meta.hpp"
+
+#include "cgiutil.hpp"
 
 namespace bookstore {
 
 RPP_VISITOR_CHAIN_INIT()
 
-RPP_VISITOR_REG(rpp::VisitorIStrTree<cgicc::FCgiCC<> &>)
-RPP_VISITOR_REG(rpp::VisitorJSON<cgicc::FCgiCC<>>)
+// RPP_VISITOR_REG(rpp::VisitorIStrTree<cgicc::FCgiCC<>>)
+// RPP_VISITOR_REG(rpp::VisitorJSON<cgicc::FCgiCC<>>)
 RPP_VISITOR_REG(rpp::VisitorBSON<>)
 RPP_VISITOR_REG(rpp::VisitorBSONView<>)
 
--- bookstore_model.hpp
+++ bookstore_model.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <memory>
+
+#include <bsoncxx/oid.hpp>
+
 #include "reflection++/accessor_infer.hpp"
 
 namespace bookstore {
@@ -8,7 +12,7 @@ RPP_ACCESSOR_INFER_INIT()
 
 struct oid_str: public bsoncxx::oid {
     oid_str(): bsoncxx::oid{bsoncxx::oid::init_tag} {}
-    oid_str(const std::string &str): bsoncxx::oid{str} {}
+    oid_str(const std::string &str): bsoncxx::oid{stdx::str_view{str}} {}
 
     operator std::string() const {
         return to_string();
@@ -22,29 +26,25 @@ struct oid_str: public bsoncxx::oid {
     }
 };
 
-struct User {
-    oid_str _id; // user_id
+template <class T>
+struct maybe: public std::unique_ptr<T> {
+    maybe(): std::unique_ptr<T>{} {}
 
-    std::string mail;
-    std::string name;
-    std::string image;
-    std::string detail;
-    std::string password;
-    bool is_admin;
-    std::string location;
-    std::string address;
+    maybe(T &&value): std::unique_ptr<T>{
+        new T{std::move(value)}
+    } {}
 
-    long bought_count;
-    long book_count;
-    long sold_count;
-
-    std::time_t date_create;
-    std::time_t date_login;
+    maybe(const T &value): std::unique_ptr<T>{
+        new T{value}
+    } {}
 };
+
+template <class T>
+RPP_TYPE_DYNAMIC_GENERIC(T, maybe<T>)
 
 struct Cat {
     oid_str _id; // cat_id
-    oid_str parent_cat_id;
+    maybe<oid_str> parent_cat_id;
 
     std::string name;
     std::string image;
@@ -54,6 +54,13 @@ struct Cat {
     long tot_book_count;
     long book_count;
 };
+
+RPP_TYPE_OBJECT(
+    __(_id) __(parent_cat_id)
+    __(name) __(image) __(detail)
+    __(cat_count) __(tot_book_count) __(book_count),
+    Cat
+)
 
 struct Book {
     oid_str _id; // book_id
@@ -72,17 +79,60 @@ struct Book {
     std::time_t date_create;
 };
 
+RPP_TYPE_OBJECT(
+    __(_id) __(owner_user_id) __(parent_cat_id)
+    __(name) __(image) __(detail) __(isbn) __(price) __(inventory)
+    __(sold_count)
+    __(date_create),
+    Book
+)
+
+struct User {
+    oid_str _id; // user_id
+
+    std::string mail;
+    std::string name;
+    std::string image;
+    std::string detail;
+    std::string password;
+    std::string location;
+    std::string address;
+
+    bool is_admin;
+    long bought_count;
+    long book_count;
+    long sold_count;
+
+    std::time_t date_create;
+    std::time_t date_login;
+};
+
+RPP_TYPE_OBJECT(
+    __(_id)
+    __(mail) __(name) __(image) __(detail) __(password) __(location) __(address)
+    __(is_admin) __(bought_count) __(book_count) __(sold_count)
+    __(date_create) __(date_login),
+    User
+)
+
 struct Buy {
     oid_str _id; // buy_id
     oid_str buyer_user_id;
     oid_str buy_book_id; // seller_user_id, book_name from buy_book_id
 
     std::string address;
-    std::string feedback;
+    maybe<std::string> feedback;
 
     std::time_t date_create;
-    std::time_t date_accept;
-    std::time_t date_done;
+    maybe<std::time_t> date_accept;
+    maybe<std::time_t> date_done;
 };
+
+RPP_TYPE_OBJECT(
+    __(_id) __(buyer_user_id) __(buy_book_id)
+    __(address) __(feedback)
+    __(date_create) __(date_accept) __(date_done),
+    Buy
+)
 
 }
