7268fe5174ef5210b6783e2cf0d767a4bb32d723
hcz <h.cz@qq.com>
2015-10-02 12:20:37 +0800

    remove hashutil.hpp

--- web++/hash.hpp
+++ web++/hash.hpp
@@ -1,10 +1,45 @@
 #pragma once
 
+#include <string>
+
+#include <openssl/rand.h>
+#include <openssl/sha.h>
 #include <bsoncxx/oid.hpp>
 
-#include "hashutil.hpp"
-
 namespace wpp {
+
+template <class Output = std::string>
+Output randStr() {
+    unsigned char buf[64];
+
+    RAND_bytes(buf, 64);
+
+    Output result;
+    for (long i = 0; i < 64; ++i) {
+        result += "0123456789abcdef"[(buf[i] >> 4) & 0xf];
+        result += "0123456789abcdef"[buf[i] & 0xf];
+    }
+
+    return result;
+}
+
+template <class Input, class Output = std::string>
+Output hashStr(const Input &value) {
+    unsigned char buf[SHA256_DIGEST_LENGTH];
+
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, value.data(), value.size());
+    SHA256_Final(buf, &sha256);
+
+    Output result;
+    for (long i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+        result += "0123456789abcdef"[(buf[i] >> 4) & 0xf];
+        result += "0123456789abcdef"[buf[i] & 0xf];
+    }
+
+    return result;
+}
 
 inline std::string saltedHash(
     const std::string &value,
